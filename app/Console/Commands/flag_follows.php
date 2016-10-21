76a624ca71e78ddbc6463c71916e3468d4d58fb8
saeed <psproot@gmail.com>
2016-10-22 01:07:44 +0330

    Renew

---
+++ app/Console/Commands/flag_follows.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Queue;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class flag_follows extends Command
+{
+    protected $signature = 'insta:flag_follows';
+    protected $description = 'Update follows list in queue';
+
+    public function handle()
+    {
+
+        $this->info('Reset queue followed_at field ...');
+        DB::table('queues')->whereNotNull('followed_at')->update(['followed_at' => null]);
+
+        $this->info('Update queue ...');
+        $follows = DB::table('follows')->get();
+
+        $counter = 0;
+        foreach($follows as $follow){
+
+            if(DB::table('queues')->where('id', $follow->id)->first()) {
+                $this->info((++$counter) . ') Follow was exist and updated : ' . $follow->id .' ['. $follow->username .']');
+                DB::table('queues')->where('id', $follow->id)->update(['followed_at' => new \DateTime()]);
+            } else {
+
+                $this->warn((++$counter) . ') Follow not exist and will be added : ' . $follow->id .' ['. $follow->username .']');
+                
+                $queue = new Queue();
+                $queue->id = $follow->id;
+                $queue->username = $follow->username;
+                $queue->created_at = new \DateTime();
+                $queue->followed_at = new \DateTime();
+                $queue->cycle = 0;
+                $queue->save();
+                
+            }
+        }
+    }
+}
